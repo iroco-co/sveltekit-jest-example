28287dcd5a18b30ed52b99304de69b296d2ae339
Bruno Thomas <bruno@barreverte.fr>
2021-07-30 18:35:58 +0200

    adds host env parameter

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,18 @@
 import axios from "axios"
-//const APP_HOST = import.meta.env.VITE_APP_HOST;
+const APP_HOST = import.meta.env.VITE_APP_HOST;
 
 export async function getName(): Promise<string> {
-	const response = await axios.get('/me');
-	return response.data.result
+	const response = await sendAction('/me');
+	return response.result
+}
+
+async function sendAction (url, config = {}) {
+		const r = await axios.request({ url: getFullUrl(url), ...config })
+		return r ? r.data : null
+}
+
+function getFullUrl (path) {
+	const base = APP_HOST ? APP_HOST.toString(): `${window.location.protocol}//${window.location.host}`
+	const url = new URL(path, base)
+	return url.href
 }
